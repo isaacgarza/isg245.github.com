ae7eb107bd4b40e49dcc07d24f7feba7623fe745
Isaac Garza <isaac_g@utexas.edu>
2015-06-19 20:17:25 -0500

    fixed the header and the our-team page. add the sticky nav bar

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,17 @@
-$( document ).ready(function() {
-
-  //
-
-});
+//$(function() {
+//    // Stick the #nav to the top of the window
+//    var nav = $('nav');
+//    var navHomeY = nav.offset().top;
+//    var $w = $(window);
+//    $w.scroll(function() {
+//        var scrollTop = $w.scrollTop();
+//        var shouldBeFixed = scrollTop > navHomeY;
+//        if (shouldBeFixed) {
+//            $('nav').addClass('fixed');
+//        }
+//        else if (!shouldBeFixed)
+//        {
+//            $('nav').removeClass('fixed');
+//        }
+//    });
+//});
