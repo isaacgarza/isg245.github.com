8b1386c318ad4f29226d2d9ee3a129318a5087ce
Isaac Garza <isaac_g@utexas.edu>
2015-06-23 01:21:30 -0500

    added all the pages

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,17 +1,5 @@
-//$(function() {
-//    // Stick the #nav to the top of the window
-//    var nav = $('nav');
-//    var navHomeY = nav.offset().top;
-//    var $w = $(window);
-//    $w.scroll(function() {
-//        var scrollTop = $w.scrollTop();
-//        var shouldBeFixed = scrollTop > navHomeY;
-//        if (shouldBeFixed) {
-//            $('nav').addClass('fixed');
-//        }
-//        else if (!shouldBeFixed)
-//        {
-//            $('nav').removeClass('fixed');
-//        }
-//    });
-//});
+$( document ).ready(function() {
+
+  //
+
+});
